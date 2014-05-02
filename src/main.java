b207e59a1cc8685ddcc69ee82f744dea7882fac3
josech636 <josech636@hotmail.com>
2014-05-02 14:58:35 +0200

    solucion

--- src/main.java
+++ src/main.java
@@ -5,12 +5,8 @@ public class main {
 		Escritor e = new Escritor("testingSourcetree");
 		System.out.println(e.getSentences());
 		
-<<<<<<< HEAD
-		System.out.println("8");
-=======
 		int calculadora = e.suma(3, 5);
 		
 		System.out.println(calculadora);
->>>>>>> probando
 	}
 }
