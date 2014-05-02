c35b123e9f220cd07fd628cd91da4af5fa183595
josech636 <josech636@hotmail.com>
2014-05-02 14:33:13 +0200

    Calculadora añadida

--- src/main.java
+++ src/main.java
@@ -4,5 +4,9 @@ public class main {
 	public static void main(String[] args) {
 		Escritor e = new Escritor("testingSourcetree");
 		System.out.println(e.getSentences());
+		
+		int calculadora = e.suma(3, 5);
+		
+		System.out.println(calculadora);
 	}
 }
