06e8f26e92a4dafb3d3e929f6d7703ec0c434489
josech636 <josech636@hotmail.com>
2014-05-02 15:01:42 +0200

    Ultimate

--- src/main.java
+++ src/main.java
@@ -4,9 +4,5 @@ public class main {
 	public static void main(String[] args) {
 		Escritor e = new Escritor("testingSourcetree");
 		System.out.println(e.getSentences());
-		
-		int calculadora = e.suma(3, 5);
-		
-		System.out.println(calculadora);
 	}
 }
