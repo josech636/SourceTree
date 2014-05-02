f4627bcc87a5a7a6f1ec8fd15b71b226991a8570
josech636 <josech636@hotmail.com>
2014-05-02 14:37:55 +0200

    Version B

--- src/main.java
+++ src/main.java
@@ -4,5 +4,7 @@ public class main {
 	public static void main(String[] args) {
 		Escritor e = new Escritor("testingSourcetree");
 		System.out.println(e.getSentences());
+		
+		System.out.println("8");
 	}
 }
